f7b2310a7d7148c03bd45e714a516e8144a03e3c
LuisLiraC <luis_ssbm@hotmail.com>
2019-07-09 01:04:23 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const { makeExecutableSchema } = require('graphql-tools')
+const express = require('express')
+const gqlMid = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// Defining the initial schema
+const typeDefs = readFileSync(
+  join(__dirname, 'lib', 'schema.graphql'),
+  'utf-8'
+)
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+app.use('/api', gqlMid({
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true
+}))
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/api`)
+})
